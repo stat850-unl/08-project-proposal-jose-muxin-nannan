d7ca1f9915d41b44385eb1c2be83396f14f664a2
zgbjunl <nannan.wang@huskers.unl.edu>
2021-10-25 10:25:03 -0500

    Exploring how value is related to different positions

---
+++ explore data_valueVS position.R
@@ -0,0 +1,17 @@
+# exploring the data
+data <- football_salaries
+head(data)
+str(data)
+
+library(ggplot2)
+a <- ggplot(data=data,aes(x=position,y=total_value))
+a+geom_boxplot()
+
+unique(data$position)
+library(tidyverse)
+meanPosition <-data %>% group_by(position)%>%
+  summarise(avg=mean(total_value))
+meanPosition <- as.data.frame(meanPosition)
+order <- order(meanPosition$avg,decreasing=T)
+# Quarterback has the highest value, the second is 4-3-defensive-end position
+meanPosition1 <- meanPosition[order,]
